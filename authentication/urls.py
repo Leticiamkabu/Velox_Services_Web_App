51a5c1f33158389c4ccbf6eceb9558b61cb01b98
Leticiamkabu <48065544+Leticiamkabu@users.noreply.github.com>
2022-04-11 16:40:30 +0100

    authentication page changes and view logics

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,9 +7,10 @@ app_name = 'auth'
 
 
 urlpatterns = [
-    path('login_user/',login_user_view, name = 'user_login'),
-    path('login_service_provider/',login_service_provider_view, name = 'service_provider_login'),
-    path('register_user/',register_user_view, name = 'user_reg'),
-    path('register_service_provider/',register_Service_Provider_view, name = 'service_provider_reg'),
+    path('user_authentication/',login_user_view, name = 'user_authentication'),
+    path('user_registration/',register_user_view, name = 'user_registration'),
+    
+    
+    
     
 ]
