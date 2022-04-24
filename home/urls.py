de2ffe21e13cc27b5fbf702cfa1d08a1ae33d419
De Dol <afoteylaryeasamuel@gmail.com>
2022-04-24 16:45:44 +0000

    serviceProviderUI

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('',index, name = "home"),
     path('user_dashboard/',user_dashboard_view, name = "user_dashboard"),
     path('service_provider_dashboard/',service_provider_dashboard_view, name = "service_provider_dashboard"),
+    path('all_services/',all_services, name = "all_services"),
+    path('service_provider/',service_provider_dashboard, name = "service_provider"),
+    path('user_requests/',user_requests, name = "user_requests"),
+    path('service_provider_profile/',service_provider_profile, name = "service_provider_profile"),
     
 ]
