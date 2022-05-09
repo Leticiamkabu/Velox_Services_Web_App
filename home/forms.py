b8ddaa715cc932407e500c4e31399b72103329a6
Leticiamkabu <48065544+Leticiamkabu@users.noreply.github.com>
2022-05-09 18:26:30 +0100

    view service details
    this changes helps display the services created.

--- home/forms.py
+++ home/forms.py
@@ -11,7 +11,7 @@ class Create_ServiceForm(ModelForm):
     service_name = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {"class": "form-control", "placeholder": "Service name"}))
     description = forms.CharField(max_length = 500, widget = forms.Textarea(attrs = {"class": "form-control", "placeholder": "Service discription"}))
     email = forms.CharField(max_length = 100, widget = forms.EmailInput(attrs = {"class": "form-control", "placeholder": "Enter Email"}))
-    phone_number = forms.CharField(max_length = 50, widget = forms.TextInput(attrs = {"class": "form-control", "placeholder": "Phone number"}))
+    phone_number = forms.CharField( widget = forms.TextInput(attrs = {"class": "form-control", "placeholder": "Phone number"}))
     image = forms.ImageField(max_length = 100, widget = forms.FileInput(attrs = {"class": "form-control", "placeholder": "Choose image"}))
     category = forms.ChoiceField(choices=Category)
     
--- home/models.py
+++ home/models.py
@@ -6,12 +6,13 @@ class Create_Service(models.Model):
     service_name = models.CharField(max_length = 100)
     description = models.TextField(max_length = 500) 
     Category =(
-        ('lv','Love'),
-        ('ht', 'Hate'),
+        ('Love','Lv'),
+        ('Hate', 'Ht'),
     )
     category = models.CharField(max_length = 100, choices = Category )
     email = models.EmailField(max_length = 100)
-    phone_number = models.IntegerField(max_length = 100)
+    phone_number = models.IntegerField()
+    # date = models.DateTimeField(auto_now_add = True)
     image = models.FileField(upload_to = 'images/',null = True, blank = True)
     
     def __str__(self):
