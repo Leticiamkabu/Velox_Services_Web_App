4f5a0c5e2597e2fd882d18769409625cd9764bb9
Leticiamkabu <48065544+Leticiamkabu@users.noreply.github.com>
2022-06-07 17:19:50 +0100

    Some changes in the app cant really remeber

--- home/forms.py
+++ home/forms.py
@@ -2,20 +2,34 @@ from django.forms import ModelForm
 from .models import *
 from .forms import *
 from django import forms
+from .models import *
 
 Category =[
-        ('LV','Love'),
-        ('HV', 'Hate'),
+        ('Salon','Salon'),
+        ('Carpentry', 'Carpentry'),
+        ('Tailoring','Tailoring'),
+        ('Construction Work','Construction Work'),
+        ('House Keeping', 'House Keeping'),
+
 ]
 class Create_ServiceForm(ModelForm):
+    id = models.CharField(primary_key = True, max_length = 200)
     service_name = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {"class": "form-control", "placeholder": "Service name"}))
     description = forms.CharField(max_length = 500, widget = forms.Textarea(attrs = {"class": "form-control", "placeholder": "Service discription"}))
     email = forms.CharField(max_length = 100, widget = forms.EmailInput(attrs = {"class": "form-control", "placeholder": "Enter Email"}))
     phone_number = forms.CharField( widget = forms.TextInput(attrs = {"class": "form-control", "placeholder": "Phone number"}))
     image = forms.ImageField(max_length = 100, widget = forms.FileInput(attrs = {"class": "form-control", "placeholder": "Choose image"}))
+    # image2 = forms.ImageField(max_length = 100, widget = forms.FileInput())
     category = forms.ChoiceField(choices=Category)
     
 
     class Meta:
         model = Create_Service
         fields = '__all__'
+
+
+class Service_provider_pic_Form(forms.ModelForm):
+
+    class Meta:
+        model = Service_provider
+        fields = ['id', 'image2']
--- home/models.py
+++ home/models.py
@@ -2,12 +2,27 @@ from django.db import models
 # from django.forms import ModelForm
 # Create your models here.
 
+
+# class Unique_identification(models.Model):
+#     created_date = models.DateTimeField(auto_now_add = True)
+
+#     @classmethod
+#     def generate(cls, prefix: str) -> str:
+#         instance = cls.objects.create()
+#         suffix = f"{instance.pk}".zfill(6)
+#         return f"{prefix}-{suffix}"
+
+
 class Create_Service(models.Model):
+    # tracking_id = Unique_identification.generate(prefix = "TI")
+    # id = models.CharField(primary_key = True, max_length = 200)
     service_name = models.CharField(max_length = 100)
     description = models.TextField(max_length = 500) 
     Category =(
-        ('LV','LOVE'),
-        ('HV', 'HATE'),
+        ('Salon','Salon'),
+        ('Carpentry', 'Carpentry'),
+        ('Tailoring','Tailoring'),
+        ('Construction Work','Construction Work'),
     )
     category = models.CharField(max_length = 100, choices = Category )
     email = models.EmailField(max_length = 100)
@@ -15,7 +30,17 @@ class Create_Service(models.Model):
     date_created = models.DateTimeField(auto_now_add = True, null = True)
     date_updated = models.DateTimeField(auto_now = True, null = True)
     image = models.FileField(upload_to = 'images/',null = True, blank = True)
+    # image2 = models.FileField(upload_to = 'images/service_provider',null = True, blank = True)
     
     def __str__(self):
         return self.service_name
+
+
+class Service_provider(models.Model):
+    id = models.CharField(primary_key = True, max_length = 200)
+    image2 = models.FileField(upload_to = 'images/service_provider',null = True, blank = True)
+
+
+    def __str__(self):
+        return self.id
 
